ecd6095eacd72056394258f0361cd6154cb46918
luisdaniel1709 <103487536+luisdaniel1709@users.noreply.github.com>
2022-04-20 21:48:50 -0400

    Update main.py

--- tguploaderv8okteto-main/main.py
+++ tguploaderv8okteto-main/main.py
@@ -206,7 +206,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'rockstar984'
+        tl_admin_user = 'Luis_Daniel_Diaz'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -455,7 +455,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 16536294
             #    api_hash = '0c780f6db3124fafbdc3dcde437e6f21'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5341312452:AAFWjIsMWNrxjcyJnSYRBN1QivpbB5ZzM_s'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
